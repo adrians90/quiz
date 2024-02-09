b198a075ede62125e69eea270ca827755c732cdc
Adrian <adriansandu9090@gmail.com>
2024-02-09 13:47:45 +0000

    on submit answers are chechked and if true text changes to green and congratulations modal is displayed

--- script.js
+++ script.js
@@ -17,9 +17,37 @@
 
 const form = document.getElementById("quiz-form")
 const answers = Array.from(document.querySelectorAll(".answer"))
+const questionItems = document.querySelectorAll(".question-item")
+const alert = document.querySelector("#alert")
 
 form.addEventListener("submit", e => {
   e.preventDefault()
+  questionItems.forEach(questionItems => {
+    questionItems.classList.add("incorrect")
+    questionItems.classList.remove("correct")
+  })
+
   const checkedAnswers = answers.filter(answer => answer.checked)
   console.log(checkedAnswers)
+  checkedAnswers.forEach(answer => {
+    const isCorrect = answer.value === "true"
+    const questionItem = answer.closest(".question-item")
+    if(isCorrect) {
+      questionItem.classList.add("correct")
+      questionItem.classList.remove("incorrect")
+    } else {
+      questionItem.classList.add("incorrect")
+      questionItem.classList.remove("correct")
+    }
+    const allTrue = checkedAnswers.every(answer => answer.value === "true")
+    const allAnswered = checkedAnswers.length === questionItems.length
+    if(allTrue && allAnswered){
+      alert.classList.add("active")
+      setTimeout(() => {
+        alert.classList.remove("active")
+      }, 1000)
+    }
+
+  })
+
 })
